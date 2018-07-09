f2fae9faa09b47f0817088df571319ba7dc6cd83
Hauke Stange <hauke.stange@jimdo.com>
2018-07-09 09:14:35 +0200

    Fix Price class

--- src/main/kotlin/de/shinythings/gettingstartedwithkotlin/shoppingcart/model/Price.kt
+++ src/main/kotlin/de/shinythings/gettingstartedwithkotlin/shoppingcart/model/Price.kt
@@ -1,21 +1,21 @@
 package de.shinythings.gettingstartedwithkotlin.shoppingcart.model
 
 data class Price(val amount: Int = 0) {
-    operator fun plus(increment: Int): Any {
+    operator fun plus(increment: Int): Price {
         return Price(this.amount + increment)
     }
 
-    operator fun plus(increment: Price): Any {
+    operator fun plus(increment: Price): Price {
         return Price(this.amount + increment.amount)
     }
 
-    operator fun minus(decrement: Int): Any {
+    operator fun minus(decrement: Int): Price {
         val newPrice = this.amount - decrement
         if (newPrice < 0) throw NegativePriceException()
         return Price(newPrice)
     }
 
-    operator fun minus(decrement: Price): Any {
+    operator fun minus(decrement: Price): Price {
         val newPrice = this.amount - decrement.amount
         if (newPrice < 0) throw NegativePriceException()
         return Price(newPrice)
--- src/main/kotlin/de/shinythings/gettingstartedwithkotlin/shoppingcart/model/ShoppingCart.kt
+++ src/main/kotlin/de/shinythings/gettingstartedwithkotlin/shoppingcart/model/ShoppingCart.kt
@@ -4,7 +4,7 @@ class ShoppingCart {
     val products: ArrayList<Product> = ArrayList()
 
     val total: Price
-        get() = Price(this.products.map { it.price.amount }.fold(0) { acc, item -> acc + item })
+        get() = this.products.fold(Price(0)) { acc, item -> acc + item.price }
 
     fun add(product: Product) {
         this.products.add(product)
